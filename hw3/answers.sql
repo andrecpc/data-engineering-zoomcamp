f117b39cc09bfcf7c221127c5a7b4721b8236393
Andrey Pedchenko <pan.sf@yandex.ru>
2022-02-05 20:46:57 +0300

    Update answers.sql

--- hw3/answers.sql
+++ hw3/answers.sql
@@ -26,3 +26,16 @@ cluster by dispatching_base_num as
 select * from `red-height-339014.trips_data_all.external_table_fhv`;
 
 select * from `red-height-339014.trips_data_all.external_table_fhv_part_clast_`;
+
+-- 4. --
+
+-- Count: 26558, Estimated data processed: 400 MB, Actual data processed: 155 MB
+
+create or replace table `red-height-339014.trips_data_all.external_table_fhv_part_clast__`
+partition by date(pickup_datetime)
+cluster by dispatching_base_num as
+select * from `red-height-339014.trips_data_all.external_table_fhv`;
+
+select count(*) from `red-height-339014.trips_data_all.external_table_fhv_part_clast__`
+where pickup_datetime between '2019-01-01' and '2019-03-31'
+and dispatching_base_num in ('B00987', 'B02060', 'B02279');
