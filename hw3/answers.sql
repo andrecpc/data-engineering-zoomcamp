6c69a98139f75e1319354ee132a08f842eacfff1
Andrey Pedchenko <pan.sf@yandex.ru>
2022-02-05 20:33:54 +0300

    Update answers.sql

--- hw3/answers.sql
+++ hw3/answers.sql
@@ -1,4 +1,4 @@
--- 1.
+-- 1. --
 
 -- 42084899
 
@@ -10,11 +10,19 @@ options (
 
 select count(1) from `red-height-339014.trips_data_all.external_table_fhv`;
 
--- 2. 
+-- 2. --
 
 -- 792
 
 select count(distinct dispatching_base_num) from `red-height-339014.trips_data_all.external_table_fhv`;
 
--- 3.
+-- 3. --
 
+-- Partition by dropoff_datetime and cluster by dispatching_base_num
+
+create or replace table `red-height-339014.trips_data_all.external_table_fhv_part_clast_`
+partition by date(dropoff_datetime)
+cluster by dispatching_base_num as
+select * from `red-height-339014.trips_data_all.external_table_fhv`;
+
+select * from `red-height-339014.trips_data_all.external_table_fhv_part_clast_`;
